d02b5917019624e101c15485b89a5121d2019e1f
sophia7320 <2060205344@qq.com>
2026-06-17 18:33:18 +0800

    reflector: 优化参数解析逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use std::env;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Result, eyre};
 use schgraph::App;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let args: Vec<_> = env::args().collect();
+    let mut args = env::args().skip(1);
 
-    let app = App::from_file(&args[2])?;
+    let mut file_path = None;
+
+    while let Some(arg) = args.next() {
+        if arg == "-f" {
+            file_path = args.next()
+        }
+    }
+    let file_path = file_path.ok_or_else(|| eyre!("用法: schgraph -f <数据文件>"))?;
+
+    let app = App::from_file(file_path.as_str())?;
 
     app.run()?;
 
